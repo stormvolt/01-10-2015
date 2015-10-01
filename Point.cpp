a08b9c3a27e21806e0cc176fcb7f9b23fe55d6dc
luis <luisnu97@hotmail.com>
2015-10-01 09:56:43 -0500

    point.cpp

---
+++ Point.cpp
@@ -0,0 +1,12 @@
+#include "Point.h"
+#include <iostream>
+
+using namespace std;
+
+
+void Point::printpoint()
+{
+	cout<<"La coordenada x del punto es :" <<x<<endl;
+	cout<<"La coordenada y del punto es :" <<y<<endl;
+}
+
