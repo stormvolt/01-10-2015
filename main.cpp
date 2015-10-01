de775306a67997d5186c8d597d961053677ea20b
luis <luisnu97@hotmail.com>
2015-10-01 09:55:48 -0500

    main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "Vector.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	Vector vec1;
+	vec1.start.x=0;
+	vec1.start.y=0;
+
+	vec1.end.x=4;
+	vec1.end.y=3;
+
+	double distance = vec1.distance();
+	cout<< distance << endl;
+	vec1.print();
+}
